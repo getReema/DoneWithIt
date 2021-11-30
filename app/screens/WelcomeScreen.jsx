48895c19cf6c2db9f237dbe1eb91ea1de24b0b41
getReema <reeroo02@gmail.com>
2021-11-30 11:46:32 +0300

    Add logo and buttons to Welcome Screen

--- app/screens/WelcomeScreen.jsx
+++ app/screens/WelcomeScreen.jsx
@@ -1,19 +1,60 @@
 import React from 'react'
-import { ImageBackground, StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, StyleSheet, Text, View, Image } from 'react-native'
 
 export default function WelcomeScreen() {
     return (
         <ImageBackground style= {styles.background}
         source= {require('../assets/background.jpg')}>
+
+<View style= {styles.loginContainer}> 
+
+<Image 
+source= {require('../assets/logo-red.png')}
+style= {styles.logo} />
+
+<Text> Sell What You Don't Need</Text>
+
+</View>
+
+            <View style={styles.loginButton}></View>
+            <View style={styles.registerButton}></View>
+
+
+
+
         </ImageBackground>
 
-        
+
     )
 }
 
 const styles = StyleSheet.create({
     background: {
         flex:1,
-    }
+        justifyContent: "flex-end",
+        alignItems: 'center'
+
+    },
+    loginButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: '#fc5c65',
+    },
+registerButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: '#4ECDC4',
+
+},
+logo:{
+    width:100,
+    height:100,
+},
+loginContainer: {
+    top: 60,
+        position: 'absolute',
+        alignItems: 'center'
+
+}
 
 })
