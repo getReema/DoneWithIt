c8368e521c8e4c11cdac62bd90302c6b65b13685
getReema <reeroo02@gmail.com>
2021-12-01 12:01:49 +0300

    Implement reusable Icon Component

--- App.js
+++ App.js
@@ -1,18 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View,ImageBackground  } from 'react-native';
 import MessageScreen from './app/screens/MessageScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import AppButton from './Components/AppButton';
 import Card from './Components/Card';
 import ListingDetailsScreen from './Components/ListingDetailsScreen';
+import Screen from './Components/Screen';
+import Icon from './Components/Icon';
 
 export default function App() {
   return (
-    <SafeAreaView>
-   <MessageScreen></MessageScreen>
-   </SafeAreaView>
+    <Screen>
+      <Icon
+        name="email"
+        size={60}
+        backgroundColor="greenyellow"
+        iconColor="white"
+
+      />
+    </Screen>
   );
 }
 
---
+++ Components/Icon.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+export default function Icon(props) {
+
+    const { name, size = 40, backgroundColor = '#000', iconColor = '#fff' } = props;
+    return (
+        <View style={{
+            width: size,
+            height: size,
+            borderRadius: size / 2,
+            backgroundColor: backgroundColor,
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <MaterialCommunityIcons name={name} color={iconColor} size={size * 0.5} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
