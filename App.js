379c29eba6fa46383c40eaf5b0b8017185773a8c
getReema <reeroo02@gmail.com>
2021-12-02 13:33:19 +0300

    Implement Passing Paramter

--- App.js
+++ App.js
@@ -28,7 +28,7 @@ const Link = () => {
   return (
     <Button
       title='Click'
-      onPress={() => navigation.navigate('TweetDetails')}
+      onPress={() => navigation.navigate('TweetDetails', { id: 1 })}
     />
   )
 }
@@ -41,9 +41,9 @@ const Tweets = ({ navigation }) => (
 </Screen>
 )
 
-const TweetDetails = () => (
+const TweetDetails = ({ route }) => (
   <Screen>
-    <Text>Tweet Details</Text>
+    <Text>Tweet Details {route.params.id} </Text>
   </Screen>
 )
 
